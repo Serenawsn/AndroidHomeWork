aa8a59a23e758dedd5e494e0fb28f16ca5ee8081
Serenawsn <Serena_weng@outlook.com>
2024-05-11 23:00:49 +0800

    feat:绑定服务和解绑服务

--- service/src/main/java/com/example/service/MainActivity.java
+++ service/src/main/java/com/example/service/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button bindBtn;
+    Button unbindBtn;
+    Button getServiceBtn;
+    private MyConn myConn;
+    private MyService.MyBinder myBinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindBtn = findViewById(R.id.bindBtn);
+        unbindBtn = findViewById(R.id.unbindBtn);
+        getServiceBtn = findViewById(R.id.getServiceBtn);
+
+        //绑定服务
+        bindBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                if (myConn==null){
+//                    myConn=new MyConn();
+//                }
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+            }
+        });
+        getServiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "service的值为：" + myBinder.getCount(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        //解绑服务
+        unbindBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                if (myConn!=null){
+                unbindService(serviceConnection);
+//                    myConn=null;
+//                }
+            }
+        });
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.v("MainActivity", "服务连接成功");
+            myBinder = (MyService.MyBinder) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.v("MainActivity", "服务断开连接");
+        }
+    };
+
+
+    private class MyConn implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.v("MainActivity", "服务绑定成功");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
     }
 }
---
+++ service/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,64 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private int count;      //充当服务的状态
+    private boolean stop;   //确定是否停止count计数
+    private MyBinder binder = new MyBinder();  //onBinder方法返回的对象
+
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        Log.v("MyService", "服务创建成功onCreate()");
+        super.onCreate();
+        new Thread() {
+            @Override
+            public void run() {
+                while (!stop) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    count++;
+
+                }
+            }
+        }.start();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        Log.v("MyService", "服务绑定成功onBind()");
+        return binder;
+//        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.v("MyService", "服务解除绑定onUnbind()");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v("MyService", "服务解除onDestroy()");
+        super.onDestroy();
+    }
+
+
+    public class MyBinder extends Binder {
+        public int getCount() {
+            //获取Service的运行状态
+            return count;
+        }
+    }
+}
